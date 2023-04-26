16e52db638b99c4b3d39ac7895fcc72d62621a16
vieanh12 <anhtvth2302006@fpt.edu.vn>
2023-04-26 10:27:06 +0700

    lab

---
+++ 26/main.js
@@ -0,0 +1,34 @@
+let id = (id) => document.getElementById(id);
+
+let classes = (classes) => document.getElementsByClassName(classes);
+
+let username = id("username"),
+    email = id("email"),
+    password = id("password"),
+    form = id("form"),
+    successIcon = classes("success-icon"),
+    failureIcon = classes("failure-icon");
+
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+
+        engine(username, 0, "Username cannot be blank");
+        engine(email, 1, "Email cannot be black");
+        engine(password, 2, "password cannot be blank");
+    });
+
+    let engine = (id, serial, message) => {
+        if (id.valie.trim() === "") {
+            errorMsg[serial].innerHTML = message;
+            id.style.border = "2px solid red";
+
+            failureIcon[serial].style.opacity = "1";
+            successIcon[serial].style.opacity = "0";
+        } else {
+            errorMsg[serial].innerHTML = "";
+            id.style.border = "2px solid green";
+
+            failureIcon[serial].style.opacity = "0";
+            successIcon[serial].style.opacity = "1";
+        }
+    };
