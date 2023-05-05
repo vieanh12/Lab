5ba49b10d1e9753f79c344d7c7da9b3b0c7d1e40
vieanh12 <anhtvth2302006@fpt.edu.vn>
2023-05-05 16:16:38 +0700

    AJS

--- yTv2.0/js/index.js
+++ yTv2.0/js/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    var inputSearch = this.documentElement("keyword");
+    var inputSearch =document.getElementById("keyword");
     inputSearch.onkeydown = function(event){
         if (event.keyCode == 13){
             loadVideo(this.value);
